b39c373c4ccb0071691fd4a3341bab4438001738
pranomostro <pranomostro@posteo.net>
2018-02-20 20:24:30 +0100

    Fix typo in config.mk.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 PREFIX = /usr
-MAXPREFIX = $(PREFIX)/share/man
+MANPREFIX = $(PREFIX)/share/man
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
 CFLAGS   = -std=c99 -Wall -Wextra -pedantic $(CPPFLAGS)
